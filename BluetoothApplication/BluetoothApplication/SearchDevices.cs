ae5017129d3ce0d59e922c693fbe397da1ad1d25
sguertl <sebastian.guertl@gmail.com>
2016-07-18 09:36:04 +0200

    Revert "Update"
    This reverts commit 128e0d0c02d4d2347081cc3b6f9d1a54d252c67e.

--- BluetoothApplication/BluetoothApplication/SearchDevices.cs
+++ BluetoothApplication/BluetoothApplication/SearchDevices.cs
@@ -53,7 +53,7 @@ namespace BluetoothApplication
             }
 
             Console.WriteLine("////////////////  " + "Device : " + btDevice.Name + "  " + m_Uuids[e.Position]);
-            ConnectedThread connect = new ConnectedThread(btDevice, m_Uuids[e.Position], this);
+            ConnectedThread connect = new ConnectedThread(btDevice, m_Uuids[e.Position]);
             Console.ReadLine();
             connect.Start();
             // PairDevice(device);
