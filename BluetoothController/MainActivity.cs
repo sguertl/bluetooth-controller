5f2113624f04aebf3ac4b74cbf0f3c5f7cd6cf72
sguertl <sebastian.guertl@gmail.com>
2016-07-19 16:44:04 +0200

    Documentation

--- BluetoothController/MainActivity.cs
+++ BluetoothController/MainActivity.cs
@@ -10,13 +10,10 @@ using Android.Graphics.Drawables;
 
 namespace BluetoothController
 {
-
-    // ------------- DOKUMENTATION, FORMATIERUNG -------------------
-
     [Activity(Label = "BluetoothController", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        // Member Variablen
+        // Members
         private BluetoothAdapter m_BtAdapter;
         private Button m_BtPairedDevices;
         private Button m_BtSearchDevices;
@@ -31,8 +28,7 @@ namespace BluetoothController
             //ಠ_ಠ
             Init();
 
-            // Fragt ab ob der BluetoothAdapter null ist
-            // Wenn dies zutrifft wird Bluetooth am jeweiligen Device nicht unterstützt
+            // Checking if bluetooth is supported
             if (m_BtAdapter == null)
             {
                 Toast.MakeText(ApplicationContext, "Bluetooth is not supported", 0).Show();
@@ -41,7 +37,7 @@ namespace BluetoothController
                 m_BtPairedDevices.Enabled = false;
                 m_BtPairedDevices.SetTextColor(Android.Graphics.Color.LightGray);
 
-                // Gibt ein Dialogfenster aus
+                // Displaying an alert to inform the user that bluetooth is not supported
                 AlertDialog alert = new AlertDialog.Builder(this).Create();
                 alert.SetTitle("Bluetooth not supported");
                 alert.SetMessage("Bluetooth is not supported!");
@@ -51,61 +47,73 @@ namespace BluetoothController
             }
             else
             {
-                // Fragt ab ob Bluetooth eingeschaltet ist
-                if (!m_BtAdapter.IsEnabled) { TurnBTOn(); } // Bluetoothaktievierungs Anfrage
+                // Checking if bluetooth is enabled
+                if (!m_BtAdapter.IsEnabled) { TurnBTOn(); }
             }
         }
 
+        /// <summary>
+        /// Initializing and modifies objects
+        /// </summary>
         public void Init()
         {
-            // Erzeugen der Objekte
+            // Initializing objects
             m_BtAdapter = BluetoothAdapter.DefaultAdapter;
             m_BtPairedDevices = FindViewById<Button>(Resource.Id.btPairedDevices);
             m_BtSearchDevices = FindViewById<Button>(Resource.Id.btSearchDevices);
             m_Linear = FindViewById<LinearLayout>(Resource.Id.linear);
 
-            // Text Color [Button]
-            m_BtPairedDevices.SetTextColor(Android.Graphics.Color.Black); // Setzt die Text Color Schwarz
-            m_BtSearchDevices.SetTextColor(Android.Graphics.Color.Black); // Setzt die Text Color Schwarz
+            // Setting text color of the buttons
+            m_BtPairedDevices.SetTextColor(Android.Graphics.Color.Black);
+            m_BtSearchDevices.SetTextColor(Android.Graphics.Color.Black);
 
-            // Border
+            // Setting border
             m_Drawable = new GradientDrawable();
-            m_Drawable.SetShape(ShapeType.Rectangle);               // Formt das Drawable Objekt in ein Rechteckt
-            m_Drawable.SetStroke(2, Android.Graphics.Color.Black);  // Setzt die Border Stärke auf 2 und die Farbe Schwarz
-            m_Drawable.SetColor(Android.Graphics.Color.White);      // Setzt die Background auf Weiß
-            m_BtPairedDevices.SetBackgroundDrawable(m_Drawable);    // Button übernimmt das Desgin des Drawable Objekt
-            m_BtSearchDevices.SetBackgroundDrawable(m_Drawable);    // Button übernimmt das Desgin des Drawable Objekt
+            m_Drawable.SetShape(ShapeType.Rectangle);
+            m_Drawable.SetStroke(2, Android.Graphics.Color.Black);
+            m_Drawable.SetColor(Android.Graphics.Color.White);
+            m_BtPairedDevices.Background = m_Drawable;
+            m_BtSearchDevices.Background = m_Drawable;
 
-            // Activity Background
-            m_Linear.SetBackgroundColor(Android.Graphics.Color.White); // Setzt die Background Color Weiß
+            // Setting activity background
+            m_Linear.SetBackgroundColor(Android.Graphics.Color.White);
 
-            // On Touch Event
+            // Handling search devices button contact
             m_BtSearchDevices.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
-        {
-            if (e2.Event.Action == MotionEventActions.Down)
-                m_BtSearchDevices.SetBackgroundColor(Android.Graphics.Color.Aquamarine); // Setzt Button Background, wenn der Button berührt wird
-            else if (e2.Event.Action == MotionEventActions.Up)
             {
-                StartActivity(typeof(SearchDevices));                // Startet eine neue Activity
-                m_BtSearchDevices.SetBackgroundDrawable(m_Drawable); // Setzt das Standart Design vom Button zurück
-            }
-        };
-
-            // On Touch Event
-            m_BtPairedDevices.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
-            {
+                // Setting background blue if button was touched
                 if (e2.Event.Action == MotionEventActions.Down)
-                    m_BtPairedDevices.SetBackgroundColor(Android.Graphics.Color.Aquamarine);  // Setzt Button Background, wenn der Button berührt wird
+                {
+                    m_BtSearchDevices.SetBackgroundColor(Android.Graphics.Color.Aquamarine); // Setzt Button Background, wenn der Button berührt wird
+                }
+                // Starting a new activity if button was released
                 else if (e2.Event.Action == MotionEventActions.Up)
                 {
-                    StartActivity(typeof(PairedDevices));                // Startet eine neue Activity
-                    m_BtPairedDevices.SetBackgroundDrawable(m_Drawable); // Setzt das Standart Design vom Button zurück
+                    StartActivity(typeof(SearchDevices));
+                    m_BtSearchDevices.SetBackgroundDrawable(m_Drawable);
                 }
             };
 
+            // Handling paired devices button contact
+            m_BtPairedDevices.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
+            {
+                // Setting background blue if button was touched
+                if (e2.Event.Action == MotionEventActions.Down)
+                { 
+                    m_BtPairedDevices.SetBackgroundColor(Android.Graphics.Color.Aquamarine);
+                }
+                // Starting a new activity if button was released
+                else if (e2.Event.Action == MotionEventActions.Up)
+                {
+                    StartActivity(typeof(PairedDevices));
+                    m_BtPairedDevices.SetBackgroundDrawable(m_Drawable);
+                }
+            };
         }
 
-        // Schaltet Bluetooth ein
+        /// <summary>
+        /// Enables bluetooth on the device
+        /// </summary>
         public void TurnBTOn()
         {
             Intent intent = new Intent(BluetoothAdapter.ActionRequestEnable);
--- BluetoothController/SearchDevices.cs
+++ BluetoothController/SearchDevices.cs
@@ -14,21 +14,18 @@ using Android.Graphics.Drawables;
 
 namespace BluetoothController
 {
-
-    // --------------- DOKUMENTATION --------------------
-
     [Activity(Label = "SearchedDevices")]
     public class SearchDevices : Activity, IEstablishConnection
     {
-        // Member Variablen
+        // Members
         private BluetoothAdapter m_BtAdapter;
-        private IntentFilter m_Filter;          // Filtert Events beim Suchen
+        private IntentFilter m_Filter; // Used to filter events when searching
         private MyBroadcastreciver m_Receiver;
         private Button m_BtSearch;
         private LinearLayout m_Linear;
         private BluetoothDevice m_Device;
         private ListView m_ListView;
-        private List<String> m_Uuids;           // enthält die UUID´s (UUID = Universally Unique Identifier)
+        private List<string> m_Uuids; // List of UUIDs
         private GradientDrawable m_Drawable;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -38,13 +35,16 @@ namespace BluetoothController
 
             Init();
 
-            m_BtAdapter.StartDiscovery(); // Startet die Suche nach Devices in der Nähe
+            // Starting to discover for devices nearby
+            m_BtAdapter.StartDiscovery();
         }
 
+        /// <summary>
+        /// Initializes and modifies object
+        /// </summary>
         public void Init()
         {
-
-            // Erzeugen der Objekte
+            // Initializing objects
             m_Uuids = new List<string>();
             m_ListView = FindViewById<ListView>(Resource.Id.listViewSearched);
             m_BtSearch = FindViewById<Button>(Resource.Id.btSearchDevices);
@@ -54,94 +54,97 @@ namespace BluetoothController
             m_Filter = new IntentFilter();
             m_Receiver = new MyBroadcastreciver(this);
 
-            // Background Color [ListView]
-            m_ListView.SetBackgroundColor(Android.Graphics.Color.Black); // Setzt die Background Color auf Schwarz
+            // Setting background color of ListView
+            m_ListView.SetBackgroundColor(Android.Graphics.Color.Black);
 
-            // Event beim Klicken auf ein ListViewItem 
+            // Adding event when clicking on a ListViewItem 
             m_ListView.ItemClick += (object sender, Android.Widget.AdapterView.ItemClickEventArgs e) => { OnItemClick(sender, e); };
+       
+            // Setting text color of button
+            m_BtSearch.SetTextColor(Android.Graphics.Color.Black);
 
-            
-            // Text Color [Button]
-            m_BtSearch.SetTextColor(Android.Graphics.Color.Black); // Setzt die Text Color Schwarz
+            // Setting border
+            m_Drawable.SetShape(ShapeType.Rectangle);
+            m_Drawable.SetStroke(2, Android.Graphics.Color.Black);
+            m_Drawable.SetColor(Android.Graphics.Color.White);
+            m_BtSearch.Background = m_Drawable;
 
-            // Border
-            m_Drawable.SetShape(ShapeType.Rectangle);              // Formt das Drawable Objekt in ein Rechteckt
-            m_Drawable.SetStroke(2, Android.Graphics.Color.Black); // Setzt die Border Stärke auf 2 und die Farbe Schwarz
-            m_Drawable.SetColor(Android.Graphics.Color.White);     // Setzt die Background auf Weiß
-            m_BtSearch.SetBackgroundDrawable(m_Drawable);          // Button übernimmt das Desgin des Drawable Objekt
-
-            // Event beim Klicken des Buttons
+            // Adding event when clicking the search button
             m_BtSearch.Touch += (object sender, Android.Views.View.TouchEventArgs e2) =>
             {
+                // Changing background if button was touched
                 if (e2.Event.Action == MotionEventActions.Down)
-                    m_BtSearch.SetBackgroundColor(Android.Graphics.Color.Aquamarine); // Setzt Button Background, wenn der Button berührt wird
+                {
+                    m_BtSearch.SetBackgroundColor(Android.Graphics.Color.Aquamarine);
+                }
+                // Starting to search when button was released
                 else if (e2.Event.Action == MotionEventActions.Up)
                 {
-                    OnSearch();                                   // Startet die Suche nach neuen Devices in der Nähe
-                    m_BtSearch.SetBackgroundDrawable(m_Drawable); // Setzt das Standart Design vom Button zurück
+                    OnSearch();
+                    m_BtSearch.Background = m_Drawable;
                 }
             };
 
+            // Setting activity background
+            m_Linear.SetBackgroundColor(Android.Graphics.Color.White);
 
-            // Activity Background
-            m_Linear.SetBackgroundColor(Android.Graphics.Color.White); // Setzt die Background Color Weiß
+            // Filtering events
+            m_Filter.AddAction(BluetoothDevice.ActionFound);
+            m_Filter.AddAction(BluetoothAdapter.ActionDiscoveryStarted);
+            m_Filter.AddAction(BluetoothAdapter.ActionDiscoveryFinished);
+            m_Filter.AddAction(BluetoothDevice.ActionUuid);
 
-            // Filtert nach bestimmten Events
-            m_Filter.AddAction(BluetoothDevice.ActionFound);               // ActionFound Event wird zum Filter hinzugefügt
-            m_Filter.AddAction(BluetoothAdapter.ActionDiscoveryStarted);   // ActionDiscoveryStarted Event wird zum Filter hinzugefügt
-            m_Filter.AddAction(BluetoothAdapter.ActionDiscoveryFinished);  // ActionDiscoveryFinished Event wird zum Filter hinzugefügt
-            m_Filter.AddAction(BluetoothDevice.ActionUuid);                // ActionUuid Event wird zum Filter hinzugefügt
-
-            // Die hinzugefügten Events werden registriert und an das Broadcast Objekt weitergeleitet
+            // Registering events and forwarding them to the broadcast object
             RegisterReceiver(m_Receiver, m_Filter);
         }
 
-        // Gibt einen Toast, der vom Parameter (message) abhängt, aus
+        /// <summary>
+        /// Displays a toast
+        /// </summary>
+        /// <param name="message">Text to display</param>
         public void GiveAMessage(String message)
         {
             Toast.MakeText(ApplicationContext, message, 0).Show();
         }
 
         /// <summary>
-        /// Startet die Suche nach neuen Devices in der Nähe
+        /// Starts to discover new devices nearby
         /// </summary>
         public void OnSearch()
         {
-            m_ListView.SetAdapter(null);  // Setzt den Adapter der ListView auf Null, damit er die alten gefunden Devices löscht
-            m_Receiver.ResetList();       // Setzt die Liste mit den gefunden Devices auf null
+            // Removing old devices
+            m_ListView.SetAdapter(null);
+            m_Receiver.ResetList();
 
-            // Die CancelDiscovery-Methode ist dafür da damit nicht beim Spammen des Buttons unnötige Suchvorgänge gestartet werden
-            m_BtAdapter.CancelDiscovery();   // Bricht die Suche nach Devices ab
-            m_BtAdapter.StartDiscovery();    // Startet die Suche nach Devices in de Nähe
+            // Avoiding multiple searches
+            m_BtAdapter.CancelDiscovery();
+            m_BtAdapter.StartDiscovery();
         }
 
        
         /// <summary>
-        /// Anzeigen der gefundenen Devices
+        /// Displays found devices
         /// </summary>
         /// <param name="l"></param>
         public void SetAdapterToListView(List<String> l)
         {
-            ArrayAdapter<String> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, l); // Wandelt die übergebene List<String> in ein Adapter Array um
-            m_ListView.SetAdapter(adapter);                             // Setzt den Adapter der ListView 
-            m_ListView.SetBackgroundColor(Android.Graphics.Color.Gray); // Setz den Background jedes Item auf Grau    
+            ArrayAdapter<String> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, l);
+            m_ListView.SetAdapter(adapter);
+            m_ListView.SetBackgroundColor(Android.Graphics.Color.Gray);
         }
 
 
-        /// <summary>
-        /// Click Event [ListView]
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         public void OnItemClick(object sender, Android.Widget.AdapterView.ItemClickEventArgs e)
         {
-            TextView view = (TextView)e.View;                        // Wandelt aus ausgewählte Item der ListView in eine TextView um
-            view.SetBackgroundColor(Android.Graphics.Color.Blue);    // Setzt den Background der TextView auf Blau
-            String address = view.Text.Split('\n')[1];               // Filter, durch eine Split-Anwendung, die Adresse des Devices
+            // Displaying the chosen item on a TextView
+            TextView view = (TextView)e.View;
+            view.SetBackgroundColor(Android.Graphics.Color.Blue);
+            String address = view.Text.Split('\n')[1];
 
-            BluetoothDevice btDevice = BluetoothAdapter.DefaultAdapter.GetRemoteDevice(address);  // Erstellt ein BluetoothDevice Objekt mithilfe der Device-Adresse
-            ConnectedThread connect = new ConnectedThread(btDevice, m_Uuids[e.Position]);         // Erstellt ein Objekt von Connection Thread mit dem Bluetooth Device und mit der jeweiligen UUID
-            connect.Start();                                                                      // Startet den Thread, um sich mit dem Device zu verbinden
+            // Creating a BluetoothDevice and a ConnectionThread object
+            BluetoothDevice btDevice = BluetoothAdapter.DefaultAdapter.GetRemoteDevice(address);
+            ConnectedThread connect = new ConnectedThread(btDevice, m_Uuids[e.Position]);
+            connect.Start();
 
             var activity2 = new Intent(this, typeof(ConnectedDevices));
             IList<String> ll = new List<string>();
@@ -149,17 +152,15 @@ namespace BluetoothController
             ll.Add(btDevice.Address);
             activity2.PutStringArrayListExtra("MyData", ll);
             StartActivity(activity2);
-
-
         }
 
         /// <summary>
-        ///  Fügt eine UUID zur Liste hinzu
+        ///  Adds a UUID to the list
         /// </summary>
-        /// <param name="uuid"></param>
+        /// <param name="uuid">UUID as String</param>
         public void AddUuid(String uuid)
         {
-            m_Uuids.Add(uuid); // Hinzufügen einer UUID zur Liste
+            m_Uuids.Add(uuid);
         }
 
         public void BuildConnection()
