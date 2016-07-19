30e23dafd19b1ce563cb91809050533e4068510d
sguertl <sebastian.guertl@gmail.com>
2016-07-19 17:23:19 +0200

    Doc

--- BluetoothController/PairedDevices.cs
+++ BluetoothController/PairedDevices.cs
@@ -13,12 +13,10 @@ using Android.Bluetooth;
 
 namespace BluetoothController
 {
-    // ---------------------- DOKUMENTATION ----------------------------
-
     [Activity(Label = "PairedDevices")]
     public class PairedDevices : Activity, IEstablishConnection
     {
-        // Member Variablen
+        // Members
         private ListView m_ListView;
         private BluetoothAdapter m_BtAdapter;
         private ICollection<BluetoothDevice> m_PairedDevice;
@@ -34,20 +32,25 @@ namespace BluetoothController
   
             Init();
 
-            // Zeigt die bereits gekoppelten Devices an
+            // Displaying the paired devices
             GetPairedDevices();
         }
 
+
         private void GetPairedDevices()
         {
-            foreach (BluetoothDevice device in m_PairedDevice) { m_List.Add(device.Name + "\n" + device.Address);}  // Durchläuft die alle gekoppelten Devices und schreibt sie auf eine Liste
-            m_Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, m_List);            // Wandelt die Liste in ein Adapter Array um
-            m_ListView.Adapter = m_Adapter;                                                                         // Setzt den Adapter der ListView 
+            // Displaying all paired devices on a ListView
+            foreach (BluetoothDevice device in m_PairedDevice) { m_List.Add(device.Name + "\n" + device.Address);}
+            m_Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, m_List);
+            m_ListView.Adapter = m_Adapter;
         }
 
+        /// <summary>
+        /// Initializes and modifies objects
+        /// </summary>
         public void Init()
         {
-            // Erzeugen der Objekte
+            // Initializing objects
             m_ListView = FindViewById<ListView>(Resource.Id.listView);
             m_Linear = FindViewById<LinearLayout>(Resource.Id.linear2);
             m_BtAdapter = BluetoothAdapter.DefaultAdapter;
@@ -55,11 +58,13 @@ namespace BluetoothController
             m_List = new List<String>();
             m_UuidList = new List<String>();
 
-            // Activity Background
-            m_Linear.SetBackgroundColor(Android.Graphics.Color.White);   // Setzt die Background Color Weiß
+            // Setting activity background
+            m_Linear.SetBackgroundColor(Android.Graphics.Color.White);
 
-            // Background Color [ListView]
-            m_ListView.SetBackgroundColor(Android.Graphics.Color.Black); // Setzt die Background Color auf Schwarz
+            // Setting background color of the ListView
+            m_ListView.SetBackgroundColor(Android.Graphics.Color.Black);
+
+            // Adding handler when clicking on a ListViewItem
             m_ListView.ItemClick += (object sender, Android.Widget.AdapterView.ItemClickEventArgs e) => { OnItemClick(sender, e); };
         }
 
@@ -73,10 +78,16 @@ namespace BluetoothController
             BuildConnection(bluetoothDevice, bluetoothDevice.GetUuids()[0].Uuid.ToString());
         }
 
+        /// <summary>
+        /// Builds a connection to a BluetoothDevice
+        /// </summary>
+        /// <param name="bluetoothDevice"></param>
+        /// <param name="uuid"></param>
         public void BuildConnection(BluetoothDevice bluetoothDevice, string uuid)
         {
-            ConnectedThread connect = new ConnectedThread(bluetoothDevice, uuid);         // Erstellt ein Objekt von Connection Thread mit dem Bluetooth Device und mit der jeweiligen UUID
-            connect.Start();                                                                      // Startet den Thread, um sich mit dem Device zu verbinden
+            // Creating a ConnectionThread object
+            ConnectedThread connect = new ConnectedThread(bluetoothDevice, uuid);
+            connect.Start();
 
             var activity2 = new Intent(this, typeof(ConnectedDevices));
             IList<String> ll = new List<string>();
