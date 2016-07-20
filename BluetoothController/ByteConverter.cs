2da17c96753ad1bef775944dc192649d6782333e
chewes99 <chewes@gmx.at>
2016-07-20 14:26:20 +0200

    Update

--- BluetoothController/ByteConverter.cs
+++ BluetoothController/ByteConverter.cs
@@ -23,16 +23,28 @@ namespace BluetoothController
         public static byte[] ConvertToByte(params Int16[] args)
         {
             byte[] bytes = new byte[4];
+            int posi = 0;
 
-            for (int i = 0; i < bytes.Length; i++)
+            //for (int i = 0; i < bytes.Length; i++)
+            //{
+            //    string binaryString = GetBinary("", args[i]);
+            //    Int16 binary = Convert.ToInt16(binaryString.PadLeft(16 - binaryString.Length, '0'));
+            //    byte currentByte = (byte) binary;
+            //    bytes[i] = currentByte;
+            //    i++;
+            //    currentByte <<= args[i];
+            //    bytes[i] = currentByte;
+            //}
+
+            for (int i = 0; i < args.Length; i++)
             {
-                string binaryString = GetBinary("", args[i]);
-                Int16 binary = Convert.ToInt16(binaryString.PadLeft(16 - binaryString.Length, '0'));
-                byte currentByte = (byte) binary;
-                bytes[i] = currentByte;
-                i++;
-                currentByte <<= args[i];
-                bytes[i] = currentByte;
+                short s = args[i];
+                byte x = (byte)s;
+                bytes[posi++] = x;
+
+                short s2 = (short)(s >> 8);
+                byte x2 = (byte)s2;
+                bytes[posi++] = x2;
             }
 
             return bytes;
