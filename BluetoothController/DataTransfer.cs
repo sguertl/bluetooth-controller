89d39552304fb2ada1042f4a77487e71ea322e6c
Roflmao100 <akk24799@gmail.com>
2016-07-21 16:06:57 +0200

    bluetooth update

--- BluetoothController/DataTransfer.cs
+++ BluetoothController/DataTransfer.cs
@@ -19,18 +19,25 @@ namespace BluetoothController
         private Sender m_Sender;
         private byte[] m_Bytes;
 
+        /// <summary>
+        /// starts reading bytes
+        /// </summary>
         public DataTransfer(Controller.ControllerView CV)
         {
             m_CV = CV;
             Init();
         }
 
-        public void Init()
+        private void Init()
         {
             m_Sender = new Sender(ConnectedThread.m_Socket);
             m_Sender.Start();
         }
 
+        /// <summary>
+        /// sends data from joystick
+        /// </summary>
+        /// <param name="args">(throttle, rotation, forward/backward, left/right)</param>
         public void Write(params Int16[] args)
         {
             m_Bytes = BluetoothController.ByteConverter.ConvertToByte(args);
