88ba1361d083a2461740138bde4fcdc52882e8e1
chewes99 <chewes@gmx.at>
2016-07-21 09:09:32 +0200

    Update

--- BluetoothController/Sender.cs
+++ BluetoothController/Sender.cs
@@ -95,9 +95,8 @@ namespace BluetoothController
             try
             {
                 //  m_OutputStream.Write(bytes, 0, bytes.Length);
-
                 for (int i = 0; i < bytes.Length; i++)
-                {int   
+                {
                     string bin = ByteConverter.GetBinary("", bytes[i]);
                     Console.WriteLine(1 + i +". Byte: " + bytes[i] + " Binär: " + bin);
                 }
