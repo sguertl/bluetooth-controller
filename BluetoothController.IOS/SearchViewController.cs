9beb8fa600989c14a2de2b9b8614295a178baf7f
sguertl <sebastian.guertl@gmail.com>
2016-07-27 16:15:09 +0200

    Updated bt search

--- BluetoothController.IOS/SearchViewController.cs
+++ BluetoothController.IOS/SearchViewController.cs
@@ -2,9 +2,9 @@ using CoreBluetooth;
 using CoreFoundation;
 using Foundation;
 using System;
+using UIKit;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using UIKit;
 using ExternalAccessory;
 
 namespace BluetoothController.IOS
@@ -12,55 +12,50 @@ namespace BluetoothController.IOS
     public partial class SearchViewController : UIViewController
     {
         private MyCBCentralManagerDelegate myManagerDelegate;
-        private List<CBPeripheral> mDiscoveredDevices;
-        private CBCentralManager mCentralManager;
-        public event EventHandler ScanTimeoutElapsed = delegate { };
-        public event EventHandler<CBDiscoveredPeripheralEventArgs> DeviceDiscovered = delegate { };
-        public event EventHandler<CBPeripheralEventArgs> DeviceConnected = delegate { };
-        public event EventHandler<CBPeripheralErrorEventArgs> DeviceDisconnected = delegate { };
+		private List<CBPeripheral> mDiscoveredDevices;
+		private CBCentralManager mCentralManager;
+		public event EventHandler ScanTimeoutElapsed = delegate { };
+		public event EventHandler<CBDiscoveredPeripheralEventArgs> DeviceDiscovered = delegate { };
+		public event EventHandler<CBPeripheralEventArgs> DeviceConnected = delegate { };
+		public event EventHandler<CBPeripheralErrorEventArgs> DeviceDisconnected = delegate { };
 
         public SearchViewController (IntPtr handle) : base (handle)
         {
-            mDiscoveredDevices = new List<CBPeripheral>();
-            mCentralManager.DiscoveredPeripheral += (object sender, CBDiscoveredPeripheralEventArgs e) =>
-            {
-                Console.WriteLine("Discovered Peripherel " + e.Peripheral.Name);
-                mDiscoveredDevices.Add(e.Peripheral);
-                DeviceDiscovered(this, e);
-            };
+			mCentralManager = new CBCentralManager (DispatchQueue.CurrentQueue);
 
-            mCentralManager.UpdatedState += (object sender, EventArgs e) =>
-            {
-                Console.WriteLine("UpdatedState: " + mCentralManager.State);
-            };
+			mDiscoveredDevices = new List<CBPeripheral> ();
+			mCentralManager.DiscoveredPeripheral += (object sender, CBDiscoveredPeripheralEventArgs e) => {
+				Console.WriteLine ("Discovered Peripherel " + e.Peripheral.Name);
+				mDiscoveredDevices.Add (e.Peripheral);
+				DeviceDiscovered (this, e);
+			};
 
-            mCentralManager.ConnectedPeripheral += (object sender, CBPeripheralEventArgs e) =>
-            {
-                Console.WriteLine("Connected Peripheral: " + e.Peripheral.Name);
-                if (!mDiscoveredDevices.Contains(e.Peripheral))
-                {
-                    mDiscoveredDevices.Add(e.Peripheral);
-                }
-                DeviceConnected(sender, e);
-            };
+			mCentralManager.UpdatedState += (object sender, EventArgs e) => {
+				Console.WriteLine ("UpdatedState: " + mCentralManager.State);
+			};
 
-            mCentralManager.DisconnectedPeripheral += (object sender, CBPeripheralErrorEventArgs e) =>
-            {
-                Console.WriteLine("Disconnected Peripheral: " + e.Peripheral.Name);
-                if (mDiscoveredDevices.Contains(e.Peripheral))
-                {
-                    mDiscoveredDevices.Remove(e.Peripheral);
-                }
-                DeviceDisconnected(sender, e);
-            };
+			mCentralManager.ConnectedPeripheral += (object sender, CBPeripheralEventArgs e) => {
+				Console.WriteLine ("Connected Peripheral: " + e.Peripheral.Name);
+				if (!mDiscoveredDevices.Contains (e.Peripheral)) {
+					mDiscoveredDevices.Add (e.Peripheral);
+				}
+				DeviceConnected (sender, e);
+			};
 
-            isScanning = false;
-            mCentralManager = new CBCentralManager(DispatchQueue.CurrentQueue);
+			mCentralManager.DisconnectedPeripheral += (object sender, CBPeripheralErrorEventArgs e) => {
+				Console.WriteLine ("Disconnected Peripheral: " + e.Peripheral.Name);
+				if (mDiscoveredDevices.Contains (e.Peripheral)) {
+					mDiscoveredDevices.Remove (e.Peripheral);
+				}
+				DeviceDisconnected (sender, e);
+			};
+
+			isScanning = false;
         }
 
-        public bool isScanning { get; set; }
+		public bool isScanning { get; set; }
 
-        public override void ViewDidLoad ()
+		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
@@ -74,45 +69,35 @@ namespace BluetoothController.IOS
 
         private void OnSearchDevices(object sender, EventArgs args)
         {
-            BeginScanningForDevices();
-        }
+			BeginScanningForDevices ();
+		}
 
-        public async Task BeginScanningForDevices()
-        {
-            Console.WriteLine("Beginn scanning");
-            mDiscoveredDevices.Clear();
-            isScanning = true;
-            mCentralManager.ScanForPeripherals(peripheralUuids: null);
+		public async Task BeginScanningForDevices ()
+		{
+			Console.WriteLine ("Beginn scanning");
+			mDiscoveredDevices.Clear ();
+			isScanning = true;
+			mCentralManager.ScanForPeripherals (peripheralUuids: null);
 
-            await Task.Delay(10000);
+			await Task.Delay (10000);
 
-            if (isScanning)
-            {
-                mCentralManager.StopScan();
-                ScanTimeoutElapsed(this, new EventArgs());
-            }
-        }
+			if (isScanning) {
+				mCentralManager.StopScan ();
+				ScanTimeoutElapsed (this, new EventArgs ());
+			}
+		}
 
-        public void StopScanningForDevices()
-        {
-            Console.WriteLine("stop scanning");
-            isScanning = false;
-            mCentralManager.StopScan();
-        }
+		public void StopScanningForDevices ()
+		{
+			Console.WriteLine ("stop scanning");
+			isScanning = false;
+			mCentralManager.StopScan ();
+		}
 
-        public void DisconnectPeripheral(CBPeripheral peripheral)
-        {
-            mCentralManager.CancelPeripheralConnection(peripheral);
-            myManagerDelegate = new MyCBCentralManagerDelegate();
-            var managerDelegate = new CBCentralManager(myManagerDelegate, DispatchQueue.CurrentQueue);
-            managerDelegate.ScanForPeripherals((CBUUID[])null);
-
-			/*CBCentralManager manager = new CBCentralManager ();
-			manager.ScanForPeripherals (CBUUID.FromString("713D0003-503E-4C75-BA94-3148F18D941E"));
-			manager.DiscoveredPeripheral += delegate {
-				Console.WriteLine ("Discovered");
-			};*/
-        }
+		public void DisconnectPeripheral (CBPeripheral peripheral)
+		{
+			mCentralManager.CancelPeripheralConnection (peripheral);
+		}
 
 		public override bool ShouldAutorotate ()
 		{
