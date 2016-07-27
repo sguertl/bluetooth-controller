1cc9eaf9610af8871ed2cad01baaaad492d0f9c5
Roflmao100 <akk24799@gmail.com>
2016-07-27 13:00:48 +0200

    bluetooth update

---
+++ BluetoothController.IOS/MyCBCentralManagerDelegate.cs
@@ -0,0 +1,32 @@
+﻿using CoreBluetooth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using Foundation;
+
+namespace BluetoothController.IOS
+{
+    public class MyCBCentralManagerDelegate : CBCentralManagerDelegate
+    {
+        public override void UpdatedState(CBCentralManager manager)
+        {
+            if(manager.State == CBCentralManagerState.PoweredOn)
+            {
+                CBUUID[] cbuuids = null;
+                manager.ScanForPeripherals(cbuuids);
+                var timer = new Timer(30000);
+                timer.Elapsed += (sender, e) => manager.StopScan();
+            }
+            else
+            {
+                Console.WriteLine("Bluetooth is not available");
+            }   
+        }
+
+        public override void DiscoveredPeripheral(CBCentralManager central, CBPeripheral peripheral, NSDictionary advertisementData, NSNumber RSSI)
+        {
+            Console.WriteLine("Discovered {0}, data {1}, RSSI {2}", peripheral.Name, advertisementData, RSSI);
+        }
+    }
+}
